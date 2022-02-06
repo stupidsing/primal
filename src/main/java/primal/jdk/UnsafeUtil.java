87dfed5589b24e6bc75be233212535111dc0d6d3
ywsing <stupidsing@gmail.com>
2022-02-06 20:28:25 +0800

    -

--- src/main/java/primal/jdk/UnsafeUtil.java
+++ src/main/java/primal/jdk/UnsafeUtil.java
@@ -5,10 +5,6 @@ import java.lang.invoke.MethodHandles;
 public class UnsafeUtil {
 
 	public <T> Class<? extends T> defineClass(Class<T> interfaceClazz, String className, byte[] bytes) {
-		return defineClass(interfaceClazz, className, bytes, null);
-	}
-
-	public <T> Class<? extends T> defineClass(Class<T> interfaceClazz, String className, byte[] bytes, Object[] array) {
 		try {
 			@SuppressWarnings("unchecked")
 			var clazz = (Class<? extends T>) MethodHandles.lookup().defineHiddenClass(bytes, true).lookupClass();
